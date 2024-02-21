10d138a75731fba94e91abc52358a8d9ffaa64da
Cesar Benitez <cefeti46@gmail.com>
2024-02-20 22:34:26 -0300

    script en javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+function calcularHidratacion() {
+    var peso = document.getElementById("peso").value;
+    var resultadoElemento = document.getElementById("resultado");
+
+    // Weight validation
+    if (isNaN(peso)) {
+        resultadoElemento.textContent = "Por favor, ingrese un valor válido para el peso.";
+        return;
+    }
+
+    // Weight convertion to a number
+    peso = parseFloat(peso);
+
+    // Calcular la hidratación según las reglas dadas
+    var hidratacion = 0;
+    if (peso >= 0 && peso <= 10) {
+        hidratacion = peso * 100;
+    } else if (peso > 10 && peso <= 20) {
+        hidratacion = 10 * 100 + (peso - 10) * 50;
+    } else if (peso > 20) {
+        hidratacion = 10 * 100 + 10 * 50 + (peso - 20) * 20;
+    }
+    else if (peso > 30) {
+
+    // Show the result
+    resultadoElemento.textContent = "La hidratación recomendada es de " + hidratacion + " cc.";
+}
