0405e5c0f254ad6202d47a9f8c716b0f0f4b0189
Cesar Benitez <cefeti46@gmail.com>
2024-02-21 11:02:24 -0300

    Cambios con css

--- script.js
+++ script.js
@@ -4,40 +4,30 @@ function calcularHidratacion(){
 
     // Weight convertion string to a float
     peso = parseFloat(peso);
-
+    if (isNaN(peso)) {
+        resultadoElemento.innerHTML = "Por favor, ingrese un valor válido para el peso.";
+        return;
+    }
     //Calculate hydration according to the given rules
     var hidratacion = 0;
-    var sc=((peso*4)+7)/(peso+90);
     var mantenimiento = 0;
     var mm2 = 0;
      // Weight validation
-     if (isNaN(peso)) {
-        resultadoElemento.innerHTML = "Por favor, ingrese un valor válido para el peso.";
-        return;
-    }else if (peso >= 0 && peso <= 10) {
+    if (peso >= 0 && peso <= 10) {
         hidratacion = peso * 100;
-        mantenimiento = hidratacion/24;
-        mm2=mantenimiento*1.5;
-     // Show the result
-        resultadoElemento.innerHTML = "La hidratación recomendada es de " + hidratacion + " cc.<br>" + " El mantinimiento diario es de " +mantenimiento +"cc/hr.<br>" + "El m+m/2 diario es de " + mm2 +"cc/hr.";
     } else if (peso > 10 && peso <= 20) {
         hidratacion = 10 * 100 + (peso - 10) * 50;
-        mantenimiento = hidratacion/24;
-        mm2=mantenimiento*1.5;
-        // Show the result
-        resultadoElemento.innerHTML = "La hidratación recomendada es de " + hidratacion + " cc.<br>" + " El mantinimiento diario es de " +mantenimiento +"cc/hr.<br>" + "El m+m/2 diario es de " + mm2 +"cc/hr.";
     } else if (peso > 20) {
         hidratacion = 10 * 100 + 10 * 50 + (peso - 20) * 20;
-        mantenimiento = hidratacion/24;
-        mm2=mantenimiento*1.5;
-        // Show the result
-        resultadoElemento.innerHTML = "La hidratación recomendada es de " + hidratacion + " cc.<br>" + " El mantinimiento diario es de " +mantenimiento +"cc/hr.<br>" + "El m+m/2 diario es de " + mm2 +"cc/hr.";
     } else if (peso > 30) {
-        hidratacion = sc*1500;
-        mantenimiento = hidratacion/24;
-        mm2=mantenimiento*1.5;
-         // Show the result
-        resultadoElemento.innerHTML = "La hidratación recomendada es de " + hidratacion + " cc.<br>" + " El mantinimiento diario es de " +mantenimiento +"cc/hr.<br>" + "El m+m/2 diario es de " + mm2 +"cc/hr.";
+        hidratacion = calculoSC(peso)*1500;
     }
- 
+    mantenimiento=hidratacion/24;
+    mm2=mantenimiento*1.5;
+ // Show the result
+ resultadoElemento.innerHTML = "La hidratación recomendada es de " + hidratacion + " cc.<br>" + " El mantinimiento diario es de " +mantenimiento +"cc/hr.<br>" + "El m+m/2 diario es de " + mm2 +"cc/hr.";
+}
+// Function to calculate SC value based on weight
+function calculoSC(peso) {
+    return (((peso*4)+7)/(peso+90));
 }
