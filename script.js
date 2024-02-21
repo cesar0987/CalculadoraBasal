39cc231a1067ff5a3617ec42f8fe7e197432de04
Cesar Benitez <cefeti46@gmail.com>
2024-02-20 23:53:59 -0300

    Nuevo scripting

--- script.js
+++ script.js
@@ -11,15 +11,30 @@ function calcularHidratacion(){
     // Weight convertion to a number
     peso = parseFloat(peso);
 
-    // Calcular la hidratación según las reglas dadas
+    //Calculate hydration according to the given rules
     var hidratacion = 0;
+    var sc=((peso*4)+7)/(peso+90);
+    var mantenimiento = 0;
+    var mm2 = 0;
     if (peso >= 0 && peso <= 10) {
         hidratacion = peso * 100;
+        mantenimiento = hidratacion/24;
+        mm2=mantenimiento*1.5;
     } else if (peso > 10 && peso <= 20) {
         hidratacion = 10 * 100 + (peso - 10) * 50;
+        mantenimiento = hidratacion/24;
+        mm2=mantenimiento*1.5;
     } else if (peso > 20) {
         hidratacion = 10 * 100 + 10 * 50 + (peso - 20) * 20;
+        mantenimiento = hidratacion/24;
+        mm2=mantenimiento*1.5;
+    } else if (peso > 20) {
+        hidratacion = sc*1500;
+        mantenimiento = hidratacion/24;
+        mm2=mantenimiento*1.5;
     }
     // Show the result
     resultadoElemento.textContent = "La hidratación recomendada es de " + hidratacion + " cc.";
+    resultadoElemento.textContent = "El mantinimiento diario es de " +mantenimiento +"cc/hr."
+    resultadoElemento.textContent = "El m+m/2 diario es de " + mm2 +"cc/hr."
 }
