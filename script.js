1d7f77fc85a28886a64cf05eb579ffa732b5acef
Cesar Benitez <cefeti46@gmail.com>
2024-02-26 07:47:35 -0300

    Cambios de css para celular

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+const CALCULAR = document.getElementById("calculadoraBuena");
+const RESULTADO = document.getElementById("resultado");
+const INPUT = document.getElementById("peso");
+CALCULAR.addEventListener("click", calcularHidratacion);
+INPUT.addEventListener("click",()=>{
+    INPUT.innerHTML = ``;
+})
 function calcularHidratacion() {
   var peso = document.getElementById("peso").value;
   var resultadoElemento = document.getElementById("resultado");
 
   // Weight convertion string to a float
   peso = parseFloat(peso);
-  if (isNaN(peso)) {
-    resultadoElemento.innerHTML =
-      "Por favor, ingrese un valor válido para el peso.";
-    return;
-  }
+
   //Calculate hydration according to the given rules
   var hidratacion = 0;
   var mantenimiento = 0;
   var mm2 = 0;
-  // Weight validation
-  if (peso >= 0 && peso <= 10) {
-    hidratacion = peso * 100;
+
+  // Weight validation and calculation
+    if(peso<=0){
+      resultadoElemento.innerHTML ="Porfavor ingresar los datos de manera correcta";
+      resultado.style.display= "block";
+    return;
+  } else if(isNaN(peso)) {
+      resultadoElemento.innerHTML ="Porfavor ingresar los datos";
+      resultado.style.display= "block";
+    return;
+  }else if (peso >= 1 && peso <= 10) {
+      hidratacion = peso * 100;
   } else if (peso > 10 && peso <= 20) {
-    hidratacion = 10 * 100 + (peso - 10) * 50;
+      hidratacion = 10 * 100 + (peso - 10) * 50;
   } else if (peso > 20) {
-    hidratacion = 10 * 100 + 10 * 50 + (peso - 20) * 20;
+      hidratacion = 10 * 100 + 10 * 50 + (peso - 20) * 20;
   } else if (peso > 30) {
-    hidratacion = calculoSC(peso) * 1500;
+      hidratacion = calculoSC(peso) * 1500;
   }
   // Calculate maintenance and m+m/2 values
   mantenimiento = hidratacion / 24;
@@ -40,10 +52,4 @@ function calcularHidratacion() {
 function calculoSC(peso) {
   return (peso * 4 + 7) / (peso + 90);
 }
-const CALCULAR = document.getElementById("calculadoraBuena");
-const RESULTADO = document.getElementById("resultado");
-const INPUT = document.getElementById("peso");
-CALCULAR.addEventListener("click", calcularHidratacion);
-INPUT.addEventListener("click", () => {
-  RESULTADO.style.display = "none";
-});
+
